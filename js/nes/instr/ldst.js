80ab14582d67c751bb054e3e08128b6102f1ce68
Hacktix <zecmad16@htlkaindorf.at>
2021-06-23 01:19:23 +0200

    Implemented LDA Instruction (immediate)

--- js/nes/instr/ldst.js
+++ js/nes/instr/ldst.js
@@ -18,6 +18,25 @@ function _ldx(loadfunc, cycle) {
 funcmap[0xA2] = _ldx.bind(this, _read8_immediate);
 
 // ----------------------------------------------------------------------
+// LDA
+// ----------------------------------------------------------------------
+function _lda(loadfunc, cycle) {
+    switch(cycle) {
+        default:
+            nextfunc = loadfunc.bind(this, _lda.bind(this, null, 1));
+            break;
+        case 1:
+            registers.a = tmp.pop();
+            registers.flag_z = registers.a === 0;
+            registers.flag_n = registers.a > 127;
+            nextfunc = fetchInstruction;
+            break;
+    }
+}
+
+funcmap[0xA9] = _lda.bind(this, _read8_immediate);
+
+// ----------------------------------------------------------------------
 // STX
 // ----------------------------------------------------------------------
 function _stx(storefunc, cycle) {
