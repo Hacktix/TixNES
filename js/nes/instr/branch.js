4d6a2ad7e686f29da8ed06285383ada49ad7b822
Hacktix <zecmad16@htlkaindorf.at>
2021-06-23 01:15:03 +0200

    Implemented BCC Instruction

--- js/nes/instr/branch.js
+++ js/nes/instr/branch.js
@@ -30,3 +30,36 @@ function _bcs(cycle) {
 }
 
 funcmap[0xB0] = _bcs;
+
+// ----------------------------------------------------------------------
+// BCC
+// ----------------------------------------------------------------------
+function _bcc(cycle) {
+    switch(cycle) {
+        default:
+            nextfunc = _read8_immediate.bind(this, _bcc.bind(this, 1));
+            break;
+        case 1:
+            if(!registers.flag_c)
+                nextfunc = _bcc.bind(this, 2);
+            else {
+                tmp.pop();
+                nextfunc = fetchInstruction;
+            }
+            break;
+        case 2:
+            let offset = e8(tmp.pop());
+            let npc = (registers.pc + offset) & 0xFFFF;
+            if((npc & 0xFF00) !== (npc & 0xFF00))
+                nextfunc = _bcc.bind(this, 3);
+            else
+                nextfunc = fetchInstruction;
+            registers.pc = npc;
+            break;
+        case 3:
+            nextfunc = fetchInstruction;
+            break;
+    }
+}
+
+funcmap[0x90] = _bcc;
