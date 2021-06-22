231f397776cc868c611b40d582efb5b54978579e
Hacktix <zecmad16@htlkaindorf.at>
2021-06-22 19:32:40 +0200

    Implemented immediate LDX Instruction

--- js/nes/instr/instrs.js
+++ js/nes/instr/instrs.js
@@ -1,6 +1,7 @@
 // Include instruction files
 include('nes/instr/memaccess.js');
 include('nes/instr/jump.js');
+include('nes/instr/load.js');
 
 // Variable Declarations
 var funcmap = { };   // Maps opcodes to function callbacks
