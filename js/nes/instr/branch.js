69240b35a2dd7b83a3a7b100fd536a01f410802a
Hacktix <zecmad16@htlkaindorf.at>
2021-06-23 01:22:10 +0200

    Implemented BEQ Instruction

--- js/nes/instr/branch.js
+++ js/nes/instr/branch.js
@@ -63,3 +63,36 @@ function _bcc(cycle) {
 }
 
 funcmap[0x90] = _bcc;
+
+// ----------------------------------------------------------------------
+// BEQ
+// ----------------------------------------------------------------------
+function _beq(cycle) {
+    switch(cycle) {
+        default:
+            nextfunc = _read8_immediate.bind(this, _beq.bind(this, 1));
+            break;
+        case 1:
+            if(registers.flag_z)
+                nextfunc = _beq.bind(this, 2);
+            else {
+                tmp.pop();
+                nextfunc = fetchInstruction;
+            }
+            break;
+        case 2:
+            let offset = e8(tmp.pop());
+            let npc = (registers.pc + offset) & 0xFFFF;
+            if((npc & 0xFF00) !== (npc & 0xFF00))
+                nextfunc = _beq.bind(this, 3);
+            else
+                nextfunc = fetchInstruction;
+            registers.pc = npc;
+            break;
+        case 3:
+            nextfunc = fetchInstruction;
+            break;
+    }
+}
+
+funcmap[0xF0] = _beq;
