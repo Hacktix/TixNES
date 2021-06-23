6855cf726a95e1d0d491c4c1563c654c425ed663
Hacktix <zecmad16@htlkaindorf.at>
2021-06-23 15:26:36 +0200

    Implemented ADC Instruction (Immediate)

--- js/nes/instr/arithmetic.js
+++ js/nes/instr/arithmetic.js
@@ -17,3 +17,26 @@ function _cmp(loadfunc, cycle) {
 }
 
 funcmap[0xC9] = _cmp.bind(this, _read8_immediate);
+
+// ----------------------------------------------------------------------
+// ADC
+// ----------------------------------------------------------------------
+function _adc(loadfunc, cycle) {
+    switch(cycle) {
+        default:
+            nextfunc = loadfunc.bind(this, _adc.bind(this, null, 1));
+            break;
+        case 1:
+            let a = tmp.pop();
+            let v = a + registers.a + registers.flag_c;
+            registers.flag_c = v > 0xFF;
+            registers.flag_z = (v & 0xFF) === 0;
+            registers.flag_v = ~(registers.a ^ a) & (registers.a ^ v) & 0x80;
+            registers.flag_n = (v & 0xFF) > 127;
+            registers.a = v;
+            nextfunc = fetchInstruction;
+            break;
+    }
+}
+
+funcmap[0x69] = _adc.bind(this, _read8_immediate);
