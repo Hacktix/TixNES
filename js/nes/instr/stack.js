e8b34b0663f781e80f49443207247e231d6ce1e5
Hacktix <zecmad16@htlkaindorf.at>
2021-06-23 02:38:59 +0200

    Implemented PHA & PLP Instructions

--- js/nes/instr/stack.js
+++ js/nes/instr/stack.js
@@ -19,6 +19,50 @@ function _php(cycle) {
 funcmap[0x08] = _php;
 
 // ----------------------------------------------------------------------
+// PHA
+// ----------------------------------------------------------------------
+function _pha(cycle) {
+    switch(cycle) {
+        default:
+            nextfunc = _pha.bind(this, 1);
+            break;
+        case 1:
+            nextfunc = _pha.bind(this, 2);
+            break;
+        case 2:
+            writeByte(0x100 + registers.s--, registers.a);
+            nextfunc = fetchInstruction;
+            break;
+    }
+}
+
+funcmap[0x48] = _pha;
+
+// ----------------------------------------------------------------------
+// PLP
+// ----------------------------------------------------------------------
+function _plp(cycle) {
+    switch(cycle) {
+        default:
+            nextfunc = _plp.bind(this, 1);
+            break;
+        case 1:
+            nextfunc = _plp.bind(this, 2);
+            break;
+        case 2:
+            registers.s++;
+            nextfunc = _plp.bind(this, 3);
+            break;
+        case 3:
+            registers.p = (readByte(0x100 + registers.s) & 0xEF) | 0x20;
+            nextfunc = fetchInstruction;
+            break;
+    }
+}
+
+funcmap[0x28] = _plp;
+
+// ----------------------------------------------------------------------
 // PLA
 // ----------------------------------------------------------------------
 function _pla(cycle) {
