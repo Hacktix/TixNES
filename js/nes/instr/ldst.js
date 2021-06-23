5d051d0c60ea2f0d01c94c1d6f742e3f1d03c304
Hacktix <zecmad16@htlkaindorf.at>
2021-06-23 22:10:59 +0200

    Implemented LDY & STY Instructions (Absolute)

--- js/nes/instr/ldst.js
+++ js/nes/instr/ldst.js
@@ -38,6 +38,7 @@ function _ldy(loadfunc, cycle) {
 
 funcmap[0xA0] = _ldy.bind(this, _read8_immediate);
 funcmap[0xA4] = _ldy.bind(this, _read8_zpage);
+funcmap[0xAC] = _ldy.bind(this, _read8_absolute);
 
 // ----------------------------------------------------------------------
 // LDA
@@ -95,6 +96,7 @@ function _sty(storefunc, cycle) {
 }
 
 funcmap[0x84] = _sty.bind(this, _write8_zpage);
+funcmap[0x8C] = _sty.bind(this, _write8_absolute);
 
 // ----------------------------------------------------------------------
 // STA
